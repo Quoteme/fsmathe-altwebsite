b979a5321bea22b521b4797a313f4fc3912368af
Luca Leon Happel <lucahappel99@gmx.de>
2023-02-02 12:45:29 +0100

    archivierungstext hinzugefügt

---
+++ archive_notice.js
@@ -0,0 +1,25 @@
+main();
+
+/**
+ * Show a notification that this post is an archived post and not
+ * up to date.
+ */
+function archive_notice() {
+  const notice = document.createElement("div");
+  notice.innerHTML = "Dies ist ein archivierter Beitrag. Bitte besuchen Sie die aktuelle Seite unter <a href='https://www.fsmathe.hhu.de/'>https://www.fsmathe.hhu.de/</a>";
+  notice.style.position = "absolute";
+  notice.style.top = "0";
+  notice.style.left = "0";
+  notice.style.width = "100%";
+  notice.style.padding = "1rem";
+  notice.style.backgroundColor = "#ff0000";
+  notice.style.color = "#ffffff";
+  notice.style.textAlign = "center";
+  notice.style.fontWeight = "bold";
+  notice.style.fontSize = "2rem";
+  document.body.insertBefore(notice, document.body.firstChild);
+}
+
+function main() {
+  archive_notice();
+}
